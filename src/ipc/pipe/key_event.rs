69c12c762d485aef498bdd7a3fb95a6ac3d8b6a8
Sergio <sergio.ivanuzzo@gmail.com>
2022-08-05 13:47:21 +0700

    feat: IC-36 - added popup components and added interactivity (#20)
    * IC-36 - refactored task to use handlers separately
    
    * IC-36 - refactored code
    
    * IC-36 - refactored code
    
    * IC-36 - added support for key events
    
    * IC-36 - added realm modal; refactored part of client code to use sync::mutex instead of tokio::mutex to avoid deadlocks
    
    * IC-36 - fixed deadlocks, refactored rest of code to use sync mutex, refactored UI tasks to spawn in separate thread, refactored some code; fixed test
    
    * IC-36 - fixed warnings

---
+++ src/ipc/pipe/key_event.rs
@@ -0,0 +1,21 @@
+use std::sync::mpsc::Sender;
+use crossterm::event::KeyEvent;
+
+use crate::ipc::pipe::types::IncomeMessageType;
+
+#[derive(Clone)]
+pub struct KeyEventIncome {
+    _sender: Sender<IncomeMessageType>,
+}
+
+impl KeyEventIncome {
+    pub fn new(sender: Sender<IncomeMessageType>) -> Self {
+        Self {
+            _sender: sender,
+        }
+    }
+
+    pub fn send_key_event(&mut self, key: KeyEvent) {
+        self._sender.send(IncomeMessageType::KeyEvent(key.modifiers, key.code)).unwrap();
+    }
+}
