bf8073f922e62d137fc2d49081da150db909d0f0
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2024-04-07 11:13:52 +0200

    fix: IC-103 - refactored errors

--- src/primary/errors/config_error.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Error, Debug)]
-pub enum ConfigError {
-    #[error("Config File is not found")]
-    NotFound,
-    #[error("Scan error")]
-    ScanError(#[source] yaml_rust::ScanError),
-}
