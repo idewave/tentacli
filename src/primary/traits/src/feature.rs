bcaeaf617e416dc1164228f78ed186e326115a21
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2024-08-20 06:14:32 +0200

    feat: IC-129 - support external opcode handlers

--- src/primary/traits/src/feature.rs
+++ src/primary/traits/src/feature.rs
@@ -1,8 +1,9 @@
+use std::collections::BTreeMap;
 use anyhow::{Result as AnyResult};
 use tokio::task::JoinHandle;
 use async_broadcast::{Receiver as BroadcastReceiver, Sender as BroadcastSender};
 
-use crate::types::HandlerOutput;
+use crate::types::{HandlerOutput, ProcessorFunction, ProcessorResult};
 
 pub trait Feature: Send {
     fn new() -> Self where Self: Sized;
@@ -12,4 +13,14 @@ pub trait Feature: Send {
         receiver: BroadcastReceiver<HandlerOutput>,
     );
     fn get_tasks(&mut self) -> AnyResult<Vec<JoinHandle<()>>>;
+    fn get_login_processors(&self) -> Vec<ProcessorFunction> {
+        vec![]
+    }
+
+    fn get_realm_processors(&self) -> Vec<ProcessorFunction> {
+        vec![]
+    }
+    fn get_one_time_handler_maps(&self) -> Vec<BTreeMap<u16, ProcessorResult>> {
+        vec![]
+    }
 }
