938d4bdb5f6dbf76f22b91bf293ea03bc8b3c8b0
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2026-04-28 23:44:05 +0200

    Fix clippy collapsible-if code scanning findings

--- src/client/types.rs
+++ src/client/types.rs
@@ -137,11 +137,11 @@ impl OrderedReceiver {
                 self.next = self.buffer.keys().next().copied();
             }
 
-            if let Some(next) = self.next {
-                if let Some(entry) = self.buffer.remove(&next) {
-                    self.next = Some(next + 1);
-                    return Ok(entry);
-                }
+            if let Some(next) = self.next
+                && let Some(entry) = self.buffer.remove(&next)
+            {
+                self.next = Some(next + 1);
+                return Ok(entry);
             }
 
             // soft-fail protection
